4d9a4f2ccb007d777205279fe0e4c8e3d55c1b8b
Rocio Jacob <rochijacob@gmail.com>
2022-11-17 17:07:20 -0300

    [Add] NFT proyect
    Followed https://www.youtube.com/watch?v=_ivIUCSOZ78&t=1827s tutorial

--- App.js
+++ App.js
@@ -1,20 +1,42 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { Text, View } from 'react-native';
+import { NavigationContainer, DefaultTheme } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import Home from './screens/Home';
+import Details from './screens/Details';
+import { useState } from 'react';
+import {useFonts} from 'expo-font'
 
-export default function App() {
+
+const {Navigator, Screen} = createStackNavigator();
+
+const theme = {
+  ...DefaultTheme,
+  colors: {
+    ...DefaultTheme.colors,
+    background: 'transparent',
+  },
+}
+
+const App = () => {
+  const [loaded] = useFonts({
+    InterBold: require('./assets/fonts/Inter-Bold.ttf'),
+    InterRegular: require('./assets/fonts/Inter-Regular.ttf'),
+    InterMedium: require('./assets/fonts/Inter-Medium.ttf'),
+    InterSemiBold: require('./assets/fonts/Inter-SemiBold.ttf'),
+    InterLight: require('./assets/fonts/Inter-Light.ttf'),
+  })
+
+  if (!loaded) return null
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Navigator screenOptions={{headerShown: false}} initialRouteName='Home'>
+        <Screen name="Home" component={Home}/>
+        <Screen name="Details" component={Details} />
+      </Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+export default App
+
